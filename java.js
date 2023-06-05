12fbdf23313749791c4b005d07d4f601c6816455
unknown <Administrator@LAB6-PC7.labs.org>
2023-06-05 14:08:09 +0500

    carwebpage

---
+++ java.js
@@ -0,0 +1,91 @@
+let menu =document.querySelector('#menubtn');
+let navbar =document.querySelector('.navbar');
+
+menu.onclick= ()=>{
+    menu.classList.toggle('fa-times'); 
+    navbar.classList.toggle('active'); 
+}
+window.addEventListener("scroll", function(){
+    var header =this.document.querySelector('.header');
+    header.classList.toggle("sticky", window.scrollY >0);  
+})
+document.querySelector('.login-btn').onclick = () =>{
+    document.querySelector('.login-form-container').classList.toggle('active');
+}
+document.querySelector('#close-login-form').onclick = () =>{
+    document.querySelector('.login-form-container').classList.remove('active');
+}
+
+var swiper = new Swiper(".vehciles-slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop:true,
+    grabCursor:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  var swiper = new Swiper(".featured-slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop:true,
+    grabCursor:true,
+    centeredSlides:true,
+   
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+  var swiper = new Swiper(".review-slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop:true,
+    grabCursor:true,
+    centeredSlides:true,
+   
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+    },
+  });
